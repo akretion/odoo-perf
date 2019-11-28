9e631e25c3a17ae753bc9448f073846b3d0619b7
Sébastien BEAU <sebastien.beau@akretion.com>
2019-11-28 19:18:05 +0100

    Add module base_no_needaction

---
+++ base_no_needaction/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base No NeedAction",
+    "summary": "Remove needaction on all odoo model",
+    "version": "8.0.1.0.0",
+    "category": "Performance",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
---
+++ base_no_needaction/models/ir_needaction_mixin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.addons.base.ir.ir_needaction import ir_needaction_mixin
+
+ir_needaction_mixin._needaction = False
