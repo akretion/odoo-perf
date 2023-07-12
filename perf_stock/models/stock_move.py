e5b11336b0fc10eda21c158b657e2f14001a5013
Sébastien BEAU <sebastien.beau@akretion.com>
2023-07-13 01:35:42 +0200

    Add perf_mrp, perf_multicompany, perf_sale_stock, perf_stock module

---
+++ perf_stock/models/stock_move.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def init(self):
+        super().init()
+        # the _compute_quantities_dict in stock/models/product.py
+        # do a complexe read_group
+        # add a specific index to improve perf (in a real customer case less 3s on
+        # so validation)
+        self.env.cr.execute(
+            """
+            CREATE INDEX IF NOT EXISTS stock_move_read_group
+            ON stock_move
+            (state, product_id, location_id, location_dest_id, date, company_id)
+            """
+        )
