98f4902ccda729b9b3cdecaf9fd3ab4216c1c951
Sébastien BEAU <sebastien.beau@akretion.com>
2023-07-13 01:35:42 +0200

    Add perf_mrp, perf_multicompany, perf_sale_stock, perf_stock module

---
+++ perf_sale_stock/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "perf sale stock",
+    "summary": "Perf sale stock optimisation",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/akretion/odoo-perf",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [],
+    "demo": [],
+}
