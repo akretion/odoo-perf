601d066b3189bda1131e84a523a82af2b249afcc
Sébastien BEAU <sebastien.beau@akretion.com>
2023-10-11 02:00:27 +0200

    Add new module and improve index

---
+++ perf_sale/models/sale_order_line.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    product_id = fields.Many2one(index=True)
