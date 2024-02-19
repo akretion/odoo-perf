3bcd1c3b58abe1688feba7b3a4023f73e37f27bc
Sébastien BEAU <sebastien.beau@akretion.com>
2024-02-19 19:38:12 +0100

    perf_product: add missing index on pricelist

---
+++ perf_product/models/product_pricelist.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class ProductPricelist(models.Model):
+    _inherit = "product.pricelist"
+
+    categ_id = fields.Many2one(index=True)
+    product_id = fields.Many2one(index=True)
+    product_tmpl_id = fields.Many2one(index=True)
+    date_start = fields.Datetime(index=True)
+    date_end = fields.Datetime(index=True)
